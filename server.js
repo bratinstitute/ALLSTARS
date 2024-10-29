fcb0e06e04695908a6f83f2d740928efe0f8627b
lisiswebsite <lisi222@gmail.com>
2024-10-28 23:36:50 -0400

    idk what the change is

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
-const path = require('path'); 
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 const elements = {
     "Leo": "Fire", "Sagittarius": "Fire", "Aries": "Fire",
